e432ab3883347354bebd73894257066109e5ae01
Pedro Henrique <p.nascimento.santos@avanade.com>
2022-05-04 15:51:31 -0300

    projeto banco digital em java

---
+++ BancoDigital/src/main/java/com/example/bancodigital/Main.java
@@ -0,0 +1,25 @@
+package com.example.bancodigital;
+
+public class Main {
+    public static void main(String[] args) {
+    Cliente pedro=new Cliente();
+    pedro.setNome("Pedro");
+
+    Conta cc= new ContaCorrente(pedro);
+
+    Conta poupanca = new ContaPoupanca(pedro);
+
+
+    cc.depositar(100);
+    cc.transferir(100,poupanca);
+
+    cc.imprimirExtrato();
+    poupanca.imprimirExtrato();
+
+
+
+
+
+
+    }
+}
